a76fd7dc697042fa7579e53d2498d8b81ccd2582
Martin Macek <martin.m53@seznam.cz>
2019-05-31 22:45:21 +0200

    landing_logo

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,15 +1,12 @@
 import React from "react";
 // import { Link, } from "gatsby";
 import Navbar from "../components/Navbar";
+import logo from "../img/logo_2.png";
 
 export const IndexPageTemplate = () => (
   <div className="landing ">
     <Navbar no_logo className="container" />
-    <img
-      src="http://lightbusters.cz/images/logo_2.png"
-      alt="lightbusters"
-      className="center"
-    />
+    <img src={logo} alt="lightbusters" className="center" />
   </div>
 );
 
