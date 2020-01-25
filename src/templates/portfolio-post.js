d9ea74ec9215561aefa17027e6d551a6482ca8ff
Vojtěch Rychnovský <rychnvoj@fel.cvut.cz>
2020-01-25 14:33:42 +0100

    Added new portfolio detail page desing

--- src/templates/portfolio-post.js
+++ src/templates/portfolio-post.js
@@ -1,15 +1,15 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from "react"
+import PropTypes from "prop-types"
 // import { kebabCase } from "lodash";
-import Helmet from "react-helmet";
+import Helmet from "react-helmet"
 import {
-  graphql
+  graphql,
   //Link
-} from "gatsby";
-import Layout from "../components/Layout";
-import Navbar from "../components/Navbar";
-import Content, { HTMLContent } from "../components/Content";
-import ImageZoom from "react-medium-image-zoom";
+} from "gatsby"
+import Layout from "../components/Layout"
+import Navbar from "../components/Navbar"
+import Content, { HTMLContent } from "../components/Content"
+import ImageZoom from "react-medium-image-zoom"
 //import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
 
 export const PortfolioPostTemplate = ({
@@ -23,11 +23,12 @@ export const PortfolioPostTemplate = ({
   coop,
   images,
   date,
-  helmet
+  helmet,
 }) => {
-  const PostContent = contentComponent || Content;
+  const PostContent = contentComponent || Content
 
-  const video_id = url.replace("https://vimeo.com/", "");
+  const video_id = url.replace("https://vimeo.com/", "")
+  const coopItems = coop != null ? coop.split(";") : []
 
   return (
     <Layout>
@@ -37,79 +38,19 @@ export const PortfolioPostTemplate = ({
 
         <div className="container content">
           <div className="columns">
-            <div className="column is-one-third mobile-section">
-              <h1 className="title is-size-3">
+            <div className="column is-three-fifths is-offset-one-fifth mobile-section">
+              <h1
+                className="title is-size-3 has-text-centered"
+                style={{ marginBottom: "3rem" }}
+              >
                 <b className="main-color">{title}</b>
               </h1>
-
-              <p>{description}</p>
-              <PostContent content={content} />
-              <div className="gradient-divider" />
-              <div
-                className="mobile-vid"
-                style={{
-                  padding: "56.25% 0 0 0",
-                  position: "relative",
-                  margin: "2rem 0rem"
-                }}
-              >
-                <iframe
-                  src={"https://player.vimeo.com/video/" + video_id}
-                  title="video"
-                  style={{
-                    position: "absolute",
-                    top: "0",
-                    left: "0",
-                    width: "100%",
-                    height: "100%"
-                  }}
-                  frameBorder="0"
-                  allow="autoplay; fullscreen"
-                  allowFullScreen
-                />
-              </div>
-              <script src="https://player.vimeo.com/api/player.js" />
-              <div style={{ marginTop: `3rem` }}>
-                {client && (
-                  <div className="title-separator">
-                    <h4>Klient</h4>
-                    <b>{client}</b>
-                  </div>
-                )}
-
-                <div className="title-separator">
-                  <h4>Vytvořeno</h4>
-                  <b>{date}</b>
-                </div>
-
-                {tags && tags.length ? (
-                  <div>
-                    <h4>Kategorie</h4>
-                    <ul className="taglist">
-                      {tags.map(tag => (
-                        <li key={tag + `tag`}>
-                          {/*<Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>*/}
-                          <b>#{tag}</b>
-                        </li>
-                      ))}
-                    </ul>{" "}
-                  </div>
-                ) : null}
-                {coop && (
-                  <div className="title-separator">
-                    <h4>Spolupráce</h4>
-                    <b>{coop}</b>
-                  </div>
-                )}
-              </div>
-            </div>
-            <div className="column is-two-third">
               <div
                 className="desktop-vid"
                 style={{
                   padding: "56.25% 0 0 0",
                   position: "relative",
-                  marginBottom: "2rem"
+                  marginBottom: "2rem",
                 }}
               >
                 <iframe
@@ -120,7 +61,7 @@ export const PortfolioPostTemplate = ({
                     top: "0",
                     left: "0",
                     width: "100%",
-                    height: "100%"
+                    height: "100%",
                   }}
                   frameBorder="0"
                   allow="autoplay; fullscreen"
@@ -129,7 +70,10 @@ export const PortfolioPostTemplate = ({
               </div>
               <script src="https://player.vimeo.com/api/player.js" />
 
-              <div className="mobile-galery" style={{ marginBottom: "3rem", color: "black"}}>
+              <div
+                className="mobile-galery"
+                style={{ marginBottom: "3rem", color: "black" }}
+              >
                 {images &&
                   images.length &&
                   images.map((galleryImage, key) => (
@@ -137,17 +81,17 @@ export const PortfolioPostTemplate = ({
                       {galleryImage.image.publicURL && (
                         <ImageZoom
                           defaultStyles={{
-                            overlay: { backgroundColor: "#000000" }
+                            overlay: { backgroundColor: "#000000" },
                           }}
                           image={{
                             src: galleryImage.image.publicURL,
                             alt: "small gallery image",
-                            className: "img"
+                            className: "img",
                           }}
                           zoomImage={{
                             src: galleryImage.image.publicURL,
                             alt: "big gallery image",
-                            style: { backgroundColor: "black" }
+                            style: { backgroundColor: "black" },
                           }}
                         />
                       )}
@@ -156,22 +100,72 @@ export const PortfolioPostTemplate = ({
               </div>
             </div>
           </div>
+          <div className="columns">
+            <div className="column is-three-fifths is-offset-one-fifth mobile-section">
+              <div className="columns">
+                <div className="column is-three-fifths">
+                  <h4>O projektu</h4>
+                  <p className="portfolio-detail-description">{description}</p>
+                  <PostContent content={content} />
+                </div>
+                <div className="column">
+                  <div>
+                    {client && (
+                      <div className="title-separator">
+                        <h4>Klient</h4>
+                        <b>{client}</b>
+                      </div>
+                    )}
+
+                    <div className="title-separator">
+                      <h4>Vytvořeno</h4>
+                      <b>{date}</b>
+                    </div>
+
+                    {tags && tags.length ? (
+                      <div>
+                        <h4>Kategorie</h4>
+                        <ul className="taglist">
+                          {tags.map(tag => (
+                            <li key={tag + `tag`}>
+                              {/*<Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>*/}
+                              <b>#{tag}</b>
+                            </li>
+                          ))}
+                        </ul>{" "}
+                      </div>
+                    ) : null}
+                    {coop && (
+                      <div className="title-separator">
+                        <h4>Spolupráce</h4>
+                        <b>
+                          {coopItems.map((coopItem, key) => (
+                            <span key={key}>{coopItem} </span>
+                          ))}
+                        </b>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </Layout>
-  );
-};
+  )
+}
 
 PortfolioPostTemplate.propTypes = {
   content: PropTypes.node.isRequired,
   contentComponent: PropTypes.func,
   description: PropTypes.string,
   title: PropTypes.string,
-  helmet: PropTypes.object
-};
+  helmet: PropTypes.object,
+}
 
 const PortfolioPost = ({ data }) => {
-  const { markdownRemark: post } = data;
+  const { markdownRemark: post } = data
 
   return (
     <PortfolioPostTemplate
@@ -195,16 +189,16 @@ const PortfolioPost = ({ data }) => {
       images={post.frontmatter.images}
       date={post.frontmatter.date}
     />
-  );
-};
+  )
+}
 
 PortfolioPost.propTypes = {
   data: PropTypes.shape({
-    markdownRemark: PropTypes.object
-  })
-};
+    markdownRemark: PropTypes.object,
+  }),
+}
 
-export default PortfolioPost;
+export default PortfolioPost
 
 export const pageQuery = graphql`
   query PortfolioPostByID($id: String!) {
@@ -227,4 +221,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
+`
