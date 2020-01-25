c533d86b294ceeddb2c9103626a201fe65a3f875
Vojtěch Rychnovský <rychnvoj@fel.cvut.cz>
2020-01-25 18:44:58 +0100

    Testing console log

--- src/cms/preview-templates/PortfolioPostPreview.js
+++ src/cms/preview-templates/PortfolioPostPreview.js
@@ -5,6 +5,8 @@ import { PortfolioPostTemplate } from "../../templates/portfolio-post"
 const PortfolioPostPreview = ({ entry, getAsset }) => {
   const data = entry.getIn(["data"]).toJS()
 
+  console.log(data)
+
   if (data) {
     return <PortfolioPostTemplate {...data} />
   } else {
